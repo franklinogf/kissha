4c8201b5866b1e4c78dab5517198ab6036f19ee9
Miguel Angel <rampabonoide@gmail.com>
2020-05-18 22:14:20 -0400

    e enter the commit message for your changes. Lines starting

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const mysql = require('../database')
 
+/*product routes*/ 
 router.get('/',(req,res)=>{
   mysql.query("SELECT * FROM categories",(err,rows,fields)=>{
      if(!err){
@@ -11,5 +12,7 @@ router.get('/',(req,res)=>{
      }
   })
 })
+
+
 
 module.exports = router

aaf572b078e8b9fdcc32749518db0e44d414bc7d
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-18 22:21:51 -0400

    testing

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const mysql = require('../database')
-
+// this is for testing porpuses
 /*product routes*/ 
 router.get('/',(req,res)=>{
   mysql.query("SELECT * FROM categories",(err,rows,fields)=>{

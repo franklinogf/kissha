8adfb32d3a3eba195f45a028cd0b374bd3e92f76
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-18 21:16:22 -0400

    added a categories route

---
+++ api/routes/categories.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+const mysql = require('../database')
+
+router.get('/',(req,res)=>{
+  mysql.query("SELECT * FROM categories",(err,rows,fields)=>{
+     if(!err){
+        res.json(rows)
+     }else{
+        console.log(err)
+     }
+  })
+})
+
+module.exports = router

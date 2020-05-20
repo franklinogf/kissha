32070173f9c81bec1207b8dcef5e587dcb494a70
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-20 15:53:56 -0400

    added categories routes and a default port

--- api/server.js
+++ api/server.js
@@ -1,27 +1,28 @@
 const express = require("express")
 const app = express()
-require("dotenv/config")
 const db = require('./database')
 const users = require('./routes/users')
 const address = require('./routes/address')
+const categories = require('./routes/categories')
 
-const port = process.env.PORT
+require("dotenv/config")
+
+const port = process.env.PORT || 5000
 
 db.authenticate()
     .then(() => console.log('DB Conectada'))
     .catch(error => console.log(error))
 
 // Middlewear
-
 app.use(express.json())
 
 // Routes
 
 app.use('/users', users)
 app.use('/address', address)
+app.use('/categories', categories)
 
 // Start Server
-
 app.listen(port, () => console.log(`
 Listing on port ${port}
 

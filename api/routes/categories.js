02a6882f73a3f2f03be38d3656fdd5653bb9d711
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-20 16:18:06 -0400

    added responses

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -5,8 +5,19 @@ const categories = require('../models/categories')
 
 router.get('/', (req, res) => {
 
-   categories.findAll().then(category => {
-      res.json(category)
+   categories.findAll().then(categories => {
+      if (categories !== null) {
+         res.json({
+            status: true,
+            message: "Ok",
+            data: categories
+         })
+      } else {
+         res.json({
+            status: false,
+            message: `No categories found`
+         })
+      }
    })
 
 })
@@ -14,32 +25,76 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
    const { id } = req.params
    categories.findByPk(id).then(category => {
-      res.json(category)
+      if (category !== null) {
+         res.json({
+            status: true,
+            message: "Ok",
+            data: category
+         })
+      } else {
+         res.json({
+            status: false,
+            message: `No categories found`
+         })
+      }
    })
 
 })
 
 router.post('/', (req, res) => {
    const data = Properties(req.body)
-   categories.create(data)
+   categories.create(data).then(err => {
+      if (err == 1) {
+         res.json({
+            status: true,
+            message: "created"
+         })
+      } else {
+         res.json({
+            status: false,
+            message: "No created"
+         })
+      }
+   })
 })
 
 router.patch('/:id', (req, res) => {
    const { id } = req.params
    const data = Properties(req.body)
-   categories.update(data, {
+   const query = categories.update(data, {
       where: { id }
-   }).then(category => {
-      res.json(category)
    })
+      .then(err => {
+         if (err == 1) {
+            res.json({
+               status: true,
+               message: "Updated"
+            })
+         } else {
+            res.json({
+               status: false,
+               message: "No updated"
+            })
+         }
+      })
 })
 
 router.delete('/:id', (req, res) => {
-   const { id } = req.params  
+   const { id } = req.params
    categories.delete({
       where: { id }
-   }).then(category => {
-      res.json(category)
+   }).then(err => {
+      if (err == 1) {
+         res.json({
+            status: true,
+            message: "Deleted"
+         })
+      } else {
+         res.json({
+            status: false,
+            message: "No deleted"
+         })
+      }
    })
 })
 

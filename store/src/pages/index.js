a5ca5ec47744910096dcef0491e743ccb6641c3a
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-21 20:18:27 -0400

    used other images

--- store/src/pages/index.js
+++ store/src/pages/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout/Layout'
 import { Row, Col, Image } from 'react-bootstrap'
-import Logo from '../components/Logo/logo.png'
+import image1 from '../images/image1.png'
+import image2 from '../images/image2.png'
+import image3 from '../images/image3.png'
 
 export default class index extends Component {
    render() {
@@ -9,9 +11,9 @@ export default class index extends Component {
          <div>
             <Layout.Main>
                <Row className="mt-5 text-center">
-                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ Logo } thumbnail /></Col>
-                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ Logo } thumbnail /></Col>
-                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ Logo } thumbnail /></Col>
+                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ image1 } thumbnail /></Col>
+                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ image2 } thumbnail /></Col>
+                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ image3 } thumbnail /></Col>
                </Row>
             </Layout.Main>
          </div>

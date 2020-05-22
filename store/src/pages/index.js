59aa572380c5faef29f4ec6fc3250c8a992b14bb
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-21 20:15:24 -0400

    made the 3 articles part

--- store/src/pages/index.js
+++ store/src/pages/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout/Layout'
+import { Row, Col, Image } from 'react-bootstrap'
+import Logo from '../components/Logo/logo.png'
 
 export default class index extends Component {
    render() {
       return (
          <div>
-           <Layout.Main>
-              Index
-           </Layout.Main>
+            <Layout.Main>
+               <Row className="mt-5 text-center">
+                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ Logo } thumbnail /></Col>
+                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ Logo } thumbnail /></Col>
+                  <Col className="mt-3" xs={ 12 } md={ 4 }><Image style={ { height: '400px' } } src={ Logo } thumbnail /></Col>
+               </Row>
+            </Layout.Main>
          </div>
       )
    }

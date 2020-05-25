9acd3446a0be7498c103963dc747dd7ba90025bd
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-25 18:30:19 -0400

    added products components

---
+++ store/src/components/Products/ProductCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card,Button } from 'react-bootstrap'
+
+import styled from 'styled-components'
+
+const StyledCard = styled(Card)`
+max-width:18rem;
+`
+const CardImage = styled(Card.Img)`
+max-width: 12rem;
+`
+ const ProductCard = (props) => {
+   return (
+        <StyledCard className="border-0 text-center mx-auto">
+          <CardImage className="rounded-0 mx-auto" src={props.src} />
+          <Card.Body>
+            <Card.Text className="text-truncate">
+              {props.name}
+            </Card.Text>
+            <Card.Title>{props.price && `$${props.price}`}</Card.Title>
+            <Button size='sm' variant="primary">Add to cart</Button>
+          </Card.Body>
+        </StyledCard>
+   )
+}
+
+export default ProductCard
---
+++ store/src/components/Products/ProductRow.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Row, Col } from "react-bootstrap"
+import ProductCard from "./ProductCard"
+import sampleImage from "../../images/sample-image1.jpg"
+
+export const ProductRow = props => {
+  const cols = props.cols || 4
+  return (
+    <Row className={`justify-content-center row-cols-1 row-cols-${cols}`}>
+      {props.products.map((product,i) => (
+        <Col key={i}>
+          <ProductCard name={product.name} price={product.price} src={sampleImage} />
+        </Col>
+      ))}
+    </Row>
+  )
+}

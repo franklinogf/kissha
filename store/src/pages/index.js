4a53ebc9d43bb747604dd2ea246618b63877ac27
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-25 18:29:47 -0400

    added a new section and initiate states

--- store/src/pages/index.js
+++ store/src/pages/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react"
 import MainLayout from "../components/Layout/MainLayout"
+import Section from "../components/Layout/Section"
 import { Row, Col, Container, Button } from "react-bootstrap"
 import ImageCard from "../components/ImageCard/ImageCard"
+import { ProductRow } from "../components/Products/ProductRow"
 
 export default class index extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      products: [1]
+    }
+  }
+
+  // componentWillMount(){
+  //   fetch("http://localhost:5000/products")
+  //   .then(data => console.log(data))
+  // }
+
   render() {
     return (
       <MainLayout>
-        <Container>
+        <Section>
           <Row className="d-flex row-cols-1 row-cols-lg-3 pb-5 mt-5">
             <Col className="mt-3 d-flex justify-content-center">
               <ImageCard
@@ -28,33 +42,29 @@ export default class index extends Component {
               />
             </Col>
           </Row>
-        </Container>
-        <Container
-          className="p-5 text-dark"
-          fluid={true}
-          style={{
-            height: 496,
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1562516710-38a6fa229b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1385&q=80)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
+        </Section>
+        <Section img="https://images.unsplash.com/photo-1562516710-38a6fa229b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1385&q=80">
           <p style={{ maxWidth: "40rem" }} className="display-4">
             This is a wider card with supporting text
           </p>
           <Button variant="primary" className="mt-3">
             Read More
           </Button>
-        </Container>
+        </Section>
 
-        <Container fluid className="bg-light p-5" style={{ height: 496 }}>
+        <Section bg="light">
           <h1>Space 1</h1>
           <p>
             Qui ipsum enim labore aliqua fugiat laboris nisi pariatur tempor
             deserunt nisi occaecat mollit ipsum.
           </p>
-        </Container>
+        </Section>
+
+        <Section>
+          <Container>
+            <ProductRow products={this.state.products}/>
+          </Container>
+        </Section>
       </MainLayout>
     )
   }

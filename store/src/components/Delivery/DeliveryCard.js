d7ccab08ed223a36fce92362bfa2eacda031c0bd
Miguel Angel <rampabonoide@gmail.com>
2020-05-28 00:19:48 -0400

    Finished the Style to DeliveryCard.js

--- store/src/components/Delivery/DeliveryCard.js
+++ store/src/components/Delivery/DeliveryCard.js
@@ -1,30 +1,49 @@
 import React from "react"
-import { Row, Col, Card } from "react-bootstrap"
+import { Row, Card, Container } from "react-bootstrap"
 import { Link } from "gatsby"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styled from "styled-components"
+import "../../scss/main-bootstrap.scss"
 
+const StyleRowIcon = styled(Row)`
+  font-size: 76px;
+  margin: 0px;
+`
+const StyleRow = styled(Row)`
+  margin: 0px;
+  justify-content: center;
+  text-align-center;
+`
 
+const StyleCardTitle = styled(Card.Text)`
+  font-size: 22px;
+  font-family: Montserrat;
+  font-weight: 400;
+`
+
+const StyledSpan = styled.span`
+letter-spacing: 0px !important;
+`
 
 export const DeliveryCard = props => {
-
- 
-    
   return (
-    <Col sm={12} md={6}>
-      <Row>
-        <FontAwesomeIcon icon={props.icon} /> 
-      </Row>
-      <Row>
-        <Card.Text>
-          <Link to="/">{props.title}</Link>
-        </Card.Text>
-      </Row>
-      <Row>
-          <p>Dummy text</p>
-      </Row>
-      <Row>
-          <span>Read More</span>
-      </Row>
-    </Col>
+    <Container fluid>
+      <StyleRowIcon className="justify-content-center py-3">
+        <FontAwesomeIcon icon={props.icon} className="text-primary" />
+      </StyleRowIcon>
+      <StyleRow className="py-3">
+        <StyleCardTitle>
+          <Link to="/" className="text-dark text-decoration-none">
+            {props.title}
+          </Link>
+        </StyleCardTitle>
+      </StyleRow>
+      <StyleRow>
+        <p className="text-center pt-2 text-black-50">{props.text}</p>
+      </StyleRow>
+      <StyleRow className="py-2">
+        <StyledSpan className="pt-2 pb-1 title">READ MORE</StyledSpan>
+      </StyleRow>
+    </Container>
   )
 }

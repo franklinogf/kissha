2942cce965f3101b2bcc8ce99167eb858a71e457
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-28 17:21:13 -0400

    re-writed

--- store/src/components/Users/ReviewCard.js
+++ store/src/components/Users/ReviewCard.js
@@ -1,28 +1,27 @@
 import React from "react"
-import { Row, Card, Col } from "react-bootstrap"
+import { Card, Media, Image } from "react-bootstrap"
 import "../../scss/main-bootstrap.scss"
-
-import styled from "styled-components"
-
-const StyledCard = styled(Card)`
-    height: 9.668rem;
-`
 
 export const ReviewCard = props => {
   return (
-    <Row className="pt-4">
-      <Col xs={12} className="pb-4">
-        <StyledCard className="p-4  bg-light font-italic _font-size-20">
-            {props.userComment}
-        </StyledCard>
-      </Col>
-      <Col xs={12} md={3} className=" d-flex justify-content-center">
-        <Card.Img src={props.userPic} className="w-75 m-0 p-0"/>
-      </Col>
-      <Col xs={12} md={9} className="pl-0">
-        <p className="m-0 pb-2 _section-title _font-size-22 text-center text-md-left">{props.userName}</p>
-  <p className="text-center text-md-left">{props.userRank}</p>
-      </Col>
-    </Row>
+    <Card className="border-0 h-100">
+      <Card.Body className="border bg-light font-italic _font-size-20">
+        <Card.Text>{props.userComment}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="bg-white">
+        <Media>
+          <Image
+            width={64}
+            height={64}
+            className="align-self-center mr-3"
+            src={props.userPic}
+          />
+          <Media.Body>
+            <h5 className="_section-title _font-size-22">{props.userName}</h5>
+            <p>{props.userRank}</p>
+          </Media.Body>
+        </Media>
+      </Card.Footer>
+    </Card>
   )
 }

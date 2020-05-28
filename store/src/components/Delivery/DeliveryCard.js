e5be6cbf9fda721fc51ffa7a051f9b9541f5f1e5
Miguel Angel <rampabonoide@gmail.com>
2020-05-27 20:42:39 -0400

    React Dependency Added: FontAwsome

--- store/src/components/Delivery/DeliveryCard.js
+++ store/src/components/Delivery/DeliveryCard.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from "react"
+import { Row, Col, Card } from "react-bootstrap"
+import { Link } from "gatsby"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export const DeliveryCard = props => {
-
-    return (
-        <div></div>
-    )
-  }
+  return (
+    <Col sm={12} md={6}>
+      <Row>
+        
+      </Row>
+      <Row>
+        <Card.Text>
+          <Link to="/">{props.title}</Link>
+        </Card.Text>
+      </Row>
+      <Row>
+          <p>Dummy text</p>
+      </Row>
+      <Row>
+          <span>Read More</span>
+      </Row>
+    </Col>
+  )
+}

6c301c869788e636a9ba2d05b5a09ea3116662e6
Miguel Angel <rampabonoide@gmail.com>
2020-05-29 03:46:10 -0400

    modified cartbutton props for ProductCard

--- store/src/components/Products/ProductCard.js
+++ store/src/components/Products/ProductCard.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Card,Button } from 'react-bootstrap'
+import React from "react"
+import { Card } from "react-bootstrap"
+import { CartButton } from "../Products/CartButton"
 
-import styled from 'styled-components'
+import styled from "styled-components"
 
 const StyledCard = styled(Card)`
-max-width:18rem;
+  max-width: 18rem;
 `
 const CardImage = styled(Card.Img)`
-max-width: 12rem;
+  max-width: 12rem;
 `
- const ProductCard = (props) => {
-   return (
-        <StyledCard className="border-0 text-center mx-auto">
-          <CardImage className="rounded-0 mx-auto" src={props.src} />
-          <Card.Body>
-            <Card.Text className="text-truncate">
-              {props.name}
-            </Card.Text>
-            <Card.Title>{props.price && `$${props.price}`}</Card.Title>
-            <Button size='sm' variant="primary" className="text-white rounded-pill _font-size-14 _font-Montserrat">Add to cart</Button>
-          </Card.Body>
-        </StyledCard>
-   )
+const ProductCard = props => {
+  return (
+    <StyledCard className="border-0 text-center mx-auto">
+      <CardImage className="rounded-0 mx-auto" src={props.src} />
+      <Card.Body>
+        <Card.Text className="text-truncate">{props.name}</Card.Text>
+        <Card.Title>{props.price && `$${props.price}`}</Card.Title>
+        <CartButton
+          size="sm"
+          variant="primary"
+          fontSize="16"
+          text="Add to cart"
+          icon
+        ></CartButton>
+      </Card.Body>
+    </StyledCard>
+  )
 }
 
 export default ProductCard

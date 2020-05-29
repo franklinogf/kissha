d121bf1a8fb84edab2e2dfb50a4821c911efc213
Miguel Angel <rampabonoide@gmail.com>
2020-05-29 03:45:31 -0400

    CartButton done

--- store/src/components/Products/CartButton.js
+++ store/src/components/Products/CartButton.js
@@ -7,10 +7,20 @@ export const CartButton = props => {
   const variant = props.variant || "primary"
   const fontSize = props.fontSize || "14"
   const text = props.text || "Insert Text"
+  const icon = props.icon
   const className = `text-white rounded-pill _font-size-${fontSize} _font-Montserrat px-4 py-2`
   return (
     <Button size={size} variant={variant} className={className}>
-        {text}
+      {icon ? (
+        <FontAwesomeIcon
+          className="shopping-cart"
+          icon={["fas", "shopping-cart"]}
+          size="1x"
+        />
+      ) : (
+        ""
+      )}
+      {icon ? `  ${text}` : text}
     </Button>
   )
 }

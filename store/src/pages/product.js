a6bb2c227a2ee9e1727dc320a9b94d65f134bd34
Miguel Angel <rampabonoide@gmail.com>
2020-05-28 23:51:30 -0400

    part of the product.js Skeleton done (Part 1/3)

--- store/src/pages/product.js
+++ store/src/pages/product.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react"
 import MainLayout from "../components/Layout/MainLayout"
-
+import Section from "../components/Layout/Section"
+import { ProductGallery } from "../components/Products/ProductGallery"
+import { ProductDetail } from "../components/Products/ProductDetail"
+import { Container, Row, Col } from "react-bootstrap"
 
 export default class product extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-      }
-    }
-
-    render(){
-        return(
-            <MainLayout>
-                <h1>This is the Product Page!</h1>
-            </MainLayout>
-            
-        )
-    }
+  render() {
+    return (
+      <MainLayout>
+        <h1>This is the Product Page!</h1>
+        <Section height={1200} padding="py-5 px-0">
+          <Container className="border border-primary">
+            <Row className="no-gutter">
+              <Col xs={12} md={6} className="border border-secondary no-gutter">
+                <ProductGallery></ProductGallery>
+              </Col>
+              <Col xs={12} md={6} className="border border-secondary">
+                <ProductDetail></ProductDetail>
+              </Col>
+            </Row>
+          </Container>
+        </Section>
+      </MainLayout>
+    )
+  }
 }

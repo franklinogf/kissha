7fe1288cc84ee84b6e9c8023c18390ac4b148beb
Miguel Angel <rampabonoide@gmail.com>
2020-05-29 03:32:21 -0400

    imported FontAwsome to product.js

--- store/src/pages/product.js
+++ store/src/pages/product.js
@@ -4,6 +4,11 @@ import Section from "../components/Layout/Section"
 import { ProductGallery } from "../components/Products/ProductGallery"
 import { ProductDetail } from "../components/Products/ProductDetail"
 import { Container, Row, Col } from "react-bootstrap"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+
+library.add(fab, fas)
 
 export default class product extends Component {
   constructor(props) {
@@ -17,12 +22,12 @@ export default class product extends Component {
         <h1>This is the Product Page!</h1>
         <Section height={1200} padding="py-5 px-0">
           <Container className="border border-primary">
-            <Row className="no-gutter">
-              <Col xs={12} md={6} className="border border-secondary no-gutter">
+            <Row>
+              <Col xs={12} md={6} className="border border-secondary">
                 <ProductGallery></ProductGallery>
               </Col>
               <Col xs={12} md={6} className="border border-secondary">
-                <ProductDetail></ProductDetail>
+                <ProductDetail className="border border-dark"></ProductDetail>
               </Col>
             </Row>
           </Container>

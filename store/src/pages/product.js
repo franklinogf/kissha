2cb39dce7ecbfacb8e8c37f57e525748410182c3
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-29 16:08:56 -0400

    added a title

--- store/src/pages/product.js
+++ store/src/pages/product.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react"
 import MainLayout from "../components/Layout/MainLayout"
 import Section from "../components/Layout/Section"
-import { ProductGallery } from "../components/Products/ProductGallery"
-import { ProductDetail } from "../components/Products/ProductDetail"
+import ProductGallery from "../components/Products/ProductGallery"
+import ProductDetail from "../components/Products/ProductDetail"
 import { Container, Row, Col } from "react-bootstrap"
+import PageTitle from "../components/Layout/PageTitle"
 
 
 export default class product extends Component {
@@ -15,6 +16,7 @@ export default class product extends Component {
   render() {
     return (
       <MainLayout>
+        <PageTitle title="Products"/>
         <Section height={1200} padding="py-5 px-0">
           <Container>
             <Row>

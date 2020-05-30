66b7255f02b8419d53a6a4316608eae505c3d7ac
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-30 14:52:51 -0400

    added a link to view the product

--- store/src/components/Products/ProductCard.js
+++ store/src/components/Products/ProductCard.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Card } from "react-bootstrap"
+import { Link } from "gatsby"
 import styled from "styled-components"
 import AddToCartButton from "../Buttons/AddToCartButton"
 
@@ -12,11 +13,17 @@ const CardImage = styled(Card.Img)`
 const ProductCard = props => {
   return (
     <StyledCard className="border-0 text-center mx-auto">
-      <CardImage className="rounded-0 mx-auto" src={props.src} />
+      <Link to="/product" state={{ productId: props.id }}>
+        <CardImage className="rounded-0 mx-auto" src={props.src} />
+      </Link>
       <Card.Body>
-        <Card.Text className="text-truncate _font-Playfair-Display _font-size-22">{props.name}</Card.Text>
-        <Card.Title className="_font-Montserrat _font-size-20 my-4">{props.price && `$${props.price}`}</Card.Title>
-        <AddToCartButton icon/>
+        <Card.Text className="text-truncate _font-Playfair-Display _font-size-22">
+          {props.name}
+        </Card.Text>
+        <Card.Title className="_font-Montserrat _font-size-20 my-4">
+          {props.price && `$${props.price}`}
+        </Card.Title>
+        <AddToCartButton icon />
       </Card.Body>
     </StyledCard>
   )

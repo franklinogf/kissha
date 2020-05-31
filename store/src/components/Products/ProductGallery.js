a5c1560810303abd8bcd7609a57324e97bb56e03
Miguel Angel <rampabonoide@gmail.com>
2020-05-31 11:33:13 -0400

    ProductGallery Done! only need to pass images as a prop, meanwhile, is using static img

--- store/src/components/Products/ProductGallery.js
+++ store/src/components/Products/ProductGallery.js
@@ -1,54 +1,44 @@
 import React, { Fragment, useState } from "react"
-import { Row, Col, Carousel, Card, Image } from "react-bootstrap"
+import { Row, Carousel, Image } from "react-bootstrap"
 import ProductThumbnail from "./ProductThumbnail"
 import styled from "styled-components"
 
-/*TEMPORAL*/
+/*TEMPORAL  REMOVE AFTER GET THE SOURCE OF THE PICS*/
 import sampleImage1 from "../../images/sample-image1.jpg"
 import sampleImage2 from "../../images/sample-image2.jpg"
 import sampleImage3 from "../../images/sample-image3.jpg"
 import sampleImage4 from "../../images/sample-image4.jpg"
+//--------------------------------------------------------
 
 const StyledImage = styled(Image)`
   width: 100%;
 `
 
-const ProductGallery = () => {
+const ProductGallery = ({imgaes}) => {
   const [index, setIndex] = useState(0)
-  const [chosen, setChosen] = useState()
-  const thumbnails = [0, 1, 2, 3]
-  const images = [sampleImage1,sampleImage2,sampleImage3,sampleImage4]
 
-  const handleSelect = e => {
-    console.log(`handle called, it was the col number ${e.target.id}`)
-    setIndex(Number(e.target.id))
-  }
+  /*TEMPORAL STATES, CHANGE AFTER GET THE SORCES OF THE PICS*/
+  const thumbnails = [0, 1, 2, 3]  //Actual count of images
+  const images = [sampleImage1, sampleImage2, sampleImage3, sampleImage4] //pass images as a prop
+  //-------------------------------------------------------------------
 
   return (
     <Fragment>
       <Row className="mb-4">
         <Carousel interval={null} indicators={false} activeIndex={index}>
-          <Carousel.Item>
-            <StyledImage src={sampleImage1} />
-          </Carousel.Item>
-          <Carousel.Item>
-            <StyledImage src={sampleImage2} />
-          </Carousel.Item>
-          <Carousel.Item>
-            <StyledImage src={sampleImage3} />
-          </Carousel.Item>
-          <Carousel.Item>
-            <StyledImage src={sampleImage4} />
-          </Carousel.Item>
+          {thumbnails.map(t => (
+            <Carousel.Item>
+              <StyledImage src={images[t]} />
+            </Carousel.Item>
+          ))}
         </Carousel>
       </Row>
       <Row xs={4} className="w-100 mt-3 mb-4">
         {thumbnails.map(t => (
-          <ProductThumbnail 
+          <ProductThumbnail
             index={t}
-            handleSelect={handleSelect}
-            active={t === chosen}
-            onClick={() => setChosen(t)}
+            active={t === index}
+            onClick={() => setIndex(t)}
             image={images[t]}
           />
         ))}

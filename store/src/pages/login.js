4519844c24248f15b8aba1cbbd8bb2e1d05b4219
Miguel Angel <rampabonoide@gmail.com>
2020-05-31 12:28:20 -0400

    page login created

---
+++ store/src/pages/login.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import MainLayout from "../components/Layout/MainLayout"
+import Section from "../components/Layout/Section"
+import PageTitle from "../components/Layout/PageTitle"
+
+export default class product extends Component {
+    state = {
+
+    }
+  
+    render() {    
+      return (
+        <MainLayout>
+            
+        </MainLayout>
+      )
+    }
+  }
+  

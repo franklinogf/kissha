af0a9392181347c1541782628ab7feaf5dc88b1c
Miguel Angel <rampabonoide@gmail.com>
2020-05-31 05:04:54 -0400

    added click effect on the gallery, now you can select an image below to change the slider

--- store/src/components/Products/ProductGallery.js
+++ store/src/components/Products/ProductGallery.js
@@ -1,33 +1,46 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 import { Row, Col, Carousel, Image } from "react-bootstrap"
+/*TEMPORAL*/ 
+import sampleImage1 from '../../images/sample-image1.jpg'
+import sampleImage2 from '../../images/sample-image2.jpg'
+import sampleImage3 from '../../images/sample-image3.jpg'
+import sampleImage4 from '../../images/sample-image4.jpg'
 
 const ProductGallery = () => {
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = e => {
+    console.log(`handle called, it was the col number ${e.target.id}`)
+    setIndex(Number(e.target.id));
+  };
+
   return (
     <Fragment>
       <Row className="mb-4">
-        <Carousel interval={null} indicators={false}>
+        <Carousel interval={null} indicators={false} activeIndex={index}>
           <Carousel.Item>
             <Image
               className="d-block w-100"
-              src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01.jpg"
+              src={sampleImage1}
             />
           </Carousel.Item>
           <Carousel.Item>
             <Image
               className="d-block w-100"
-              src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01-1.jpg"
+              src={sampleImage2}
             />
           </Carousel.Item>
           <Carousel.Item>
             <Image
               className="d-block w-100"
-              src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01.jpg"
+              src={sampleImage3}
             />
           </Carousel.Item>
           <Carousel.Item>
             <Image
               className="d-block w-100"
-              src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01-3.jpg"
+              src={sampleImage4}
             />
           </Carousel.Item>
         </Carousel>
@@ -35,26 +48,30 @@ const ProductGallery = () => {
       <Row xs={4} className="w-100 mt-3 mb-4">
         <Col>
           <Image
+           id={0} onMouseDown={handleSelect}
             className="w-100"
-            src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01.jpg"
+            src={sampleImage1}
           />
         </Col>
         <Col>
           <Image
+            id={1} onMouseDown={handleSelect}
             className="w-100"
-            src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01-1.jpg"
+            src={sampleImage2}
           />
         </Col>
         <Col>
           <Image
+           id={2} onMouseDown={handleSelect}
             className="w-100"
-            src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01.jpg"
+            src={sampleImage3}
           />
         </Col>
         <Col>
           <Image
+           id={3} onMouseDown={handleSelect}
             className="w-100"
-            src="https://template65588.motopreview.com/mt-demo/65500/65588/mt-content/uploads/2018/02/mt-1349_products_img01-3.jpg"
+            src={sampleImage4}
           />
         </Col>
       </Row>

3a37e30f0f91763d5a8c3a2c8ea5ade9f2b4673a
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-30 22:45:20 -0400

    pass the product object as a prop instead of its properties and  made the sale just appears when it is on sale

--- store/src/components/Products/ProductCard.js
+++ store/src/components/Products/ProductCard.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 import AddToCartButton from "../Buttons/AddToCartButton"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import sampleImage from "../../images/sample-image1.jpg"
 
 const StyledCard = styled(Card)`
   max-width: 16.875rem;
@@ -37,13 +38,14 @@ const IconContainer = styled.div`
   position: absolute;
   z-index: 2;
   top: 50%;
-  left: 41%;
-  width: 2.5rem;
-  height: 2rem;
+  left: 50%;
+  width: 2rem;
+  height: 1.5rem;
   background-color: var(--primary);
   visibility: hidden;
   opacity: 0;
   transition: visibility 0s, opacity 0.4s linear;
+  transform: translate(-50%,-50%);
 
   ${StyledLink}:hover & {
     visibility: visible;
@@ -56,31 +58,35 @@ const StyledSale = styled.p`
   z-index: 0;  
   top:.5rem;
   left: .8rem;
-  width: 2.5rem;
-  height: 1.5rem;
+  width: fit-content;
+  height: fit-content;
+  padding: 0 .15rem;
+  margin-bottom: 0;
+  font-size: .8rem;
   color:white;
   background-color: var(--primary);
 `
-const ProductCard = props => {
+const ProductCard = ({product}) => {
+  console.log('product: ', product);
   return (
     <StyledCard className="border-0 text-center mx-auto">
-      <StyledLink to="/product" state={{ productId: props.id }}>
-        <StyledSale>SALE</StyledSale>
-        <CardImage className="rounded-0 mx-auto" src={props.src} />
+      <StyledLink to="/product" state={{ productId: product.id }}>
+        {!!product.discountStatus && <StyledSale>SALE</StyledSale>}
+        <CardImage className="rounded-0 mx-auto" src={sampleImage} />
         <IconContainer>
           <FontAwesomeIcon
             className="text-white"
             icon={["fas", "eye"]}
-            size="2x"
+            size="lg"
           />
         </IconContainer>
       </StyledLink>
       <Card.Body>
         <Card.Text className="text-truncate _font-Playfair-Display _font-size-22">
-          {props.name}
+          {product.name}
         </Card.Text>
         <Card.Title className="_font-Montserrat _font-size-20 my-4">
-          {props.price && `$${props.price}`}
+          {product.price && `$${product.price}`}
         </Card.Title>
         <AddToCartButton icon />
       </Card.Body>
--- store/src/components/Products/ProductRow.js
+++ store/src/components/Products/ProductRow.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Row, Col } from "react-bootstrap"
 import ProductCard from "./ProductCard"
-import sampleImage from "../../images/sample-image1.jpg"
 
 const ProductRow = props => {
   const cols = props.cols || 4
@@ -9,7 +8,7 @@ const ProductRow = props => {
     <Row className={`justify-content-center row-cols-1 row-cols-md-${cols}`}>
       {props.products.map((product,i) => (
         <Col className="mb-sm-4" key={i}>
-          <ProductCard id={product.id} name={product.name} price={product.price} src={sampleImage} />
+          <ProductCard product={product} />
         </Col>
       ))}
     </Row>

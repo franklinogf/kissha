26e8ff54ef82c9303637f190014dac8ad959b757
Miguel Angel <rampabonoide@gmail.com>
2020-05-31 17:13:39 -0400

    page login almost done

--- store/src/pages/login.js
+++ store/src/pages/login.js
@@ -2,18 +2,30 @@ import React, { Component } from "react"
 import MainLayout from "../components/Layout/MainLayout"
 import Section from "../components/Layout/Section"
 import PageTitle from "../components/Layout/PageTitle"
+import { Row, Col } from "react-bootstrap"
+import Login from "../components/Users/Login"
+import wallpaper from '../images/login-wallpaper.jpg'
 
 export default class product extends Component {
-    state = {
+  state = {}
 
-    }
-  
-    render() {    
-      return (
-        <MainLayout>
-            
-        </MainLayout>
-      )
-    }
+  render() {
+    return (
+      <>
+        <PageTitle title="Login" />
+        <Section img={wallpaper}>
+          
+            <Row className="pb-3 pt-2 pl-0 m-0">
+              
+              <Col xs={12} sm={4} className="py-4 pl-0">
+                <Login />
+              </Col>
+              <Col xs={12} sm={7} className=""></Col>
+              
+            </Row>
+          
+        </Section>
+      </>
+    )
   }
-  
+}

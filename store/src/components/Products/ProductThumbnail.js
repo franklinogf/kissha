f15e0bc7ee9bf3ef40dd8ef7bb34cc28bf088b0c
Miguel Angel <rampabonoide@gmail.com>
2020-05-31 11:30:47 -0400

    ProductThumbnail Done!

--- store/src/components/Products/ProductThumbnail.js
+++ store/src/components/Products/ProductThumbnail.js
@@ -2,45 +2,40 @@ import React from "react"
 import { Col, Image } from "react-bootstrap"
 import styled from "styled-components"
 
-const SCol = styled(Col)`
-  position: relative;
-
-  &:after {
-    border: 1px solid var(--primary);
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background: rgba(255, 255, 255, 0.5);
-    opacity: 0;
-    transition: all 0.2s linear;
-  }
-
-  &:hover:after {
-    opacity: 1;
-  }
-
-  &:focus:after {
-    opacity: 1;
-  }
-`
 const StyledImage = styled(Image)`
+width: 100%;
+`
+const SCol = styled(Col)`
+position: relative;
+
+&:after {
+  content: "";
+  position: absolute;
   width: 100%;
+  height: 100%;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background: rgba(255, 255, 255, 0.5);
+  opacity: 0;
+  transition: all 0.2s linear;
+}
+
+${({ active }) => !active && `
+&:hover:after {
+    opacity: 1;
+`}
 `
 
 const ProductThumbnail = ({ active, index, onClick, handleSelect, image }) => {
- 
   return (
     <SCol
+      active={active}
       id={index}
       onClick={onClick}
-      onMouseDown={handleSelect}
-      className={active && "active"}
+      className={active && "selected-thumb"}
     >
-            <StyledImage src={image} />
+      <StyledImage src={image} />
     </SCol>
   )
 }

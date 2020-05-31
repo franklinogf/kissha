4279372c881717ff78e431a04ace696415a7f170
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-30 22:54:57 -0400

    2 cols when md screen

--- store/src/components/Products/ProductRow.js
+++ store/src/components/Products/ProductRow.js
@@ -5,7 +5,7 @@ import ProductCard from "./ProductCard"
 const ProductRow = props => {
   const cols = props.cols || 4
   return (
-    <Row className={`justify-content-center row-cols-1 row-cols-md-${cols}`}>
+    <Row className={`justify-content-center row-cols-1 row-cols-md-2 row-cols-lg-${cols}`}>
       {props.products.map((product,i) => (
         <Col className="mb-sm-4" key={i}>
           <ProductCard product={product} />

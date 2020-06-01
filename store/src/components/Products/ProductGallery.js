0cf1597293a29a8f1fac0dcfc34b881121eecc4e
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-31 22:49:19 -0400

    added a key to the lists

--- store/src/components/Products/ProductGallery.js
+++ store/src/components/Products/ProductGallery.js
@@ -9,11 +9,11 @@ import sampleImage3 from "../../images/sample-image3.jpg"
 import sampleImage4 from "../../images/sample-image4.jpg"
 //--------------------------------------------------------
 
-const ProductGallery = ({imgaes}) => {
+const ProductGallery = () => {
   const [index, setIndex] = useState(0)
 
   /*TEMPORAL STATES, CHANGE AFTER GET THE SORCES OF THE PICS*/
-  const thumbnails = [0, 1, 2, 3]  //Actual count of images
+  const thumbnails = [0, 1, 2, 3] //Actual count of images
   const images = [sampleImage1, sampleImage2, sampleImage3, sampleImage4] //pass images as a prop
   //-------------------------------------------------------------------
 
@@ -21,18 +21,19 @@ const ProductGallery = ({imgaes}) => {
     <Fragment>
       <Row className="mb-4">
         <Carousel interval={null} indicators={false} activeIndex={index}>
-          {thumbnails.map(t => (
-            <Carousel.Item>
+          {thumbnails.map((t, i) => (
+            <Carousel.Item key={i}>
               <Image src={images[t]} className="w-100" />
             </Carousel.Item>
           ))}
         </Carousel>
       </Row>
       <Row xs={4} className="w-100 mt-3 mb-4">
-        {thumbnails.map(t => (
+        {thumbnails.map((t, i) => (
           <ProductThumbnail
+            key={i}
             index={t}
-            active={t === index}
+            active={t === index ? 1 : 0}
             onClick={() => setIndex(t)}
             image={images[t]}
           />

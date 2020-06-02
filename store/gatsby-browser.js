30d5ee5c2e17bc7be2e7c76852c8f2bfb34e5d9b
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-06-01 23:10:08 -0400

    import  batchingForReactDom (was required for mobx)

--- store/gatsby-browser.js
+++ store/gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from "react"
 import MainLayout from './src/components/Layout/MainLayout'
 import './src/scss/main-bootstrap.scss'
-
+import 'mobx-react-lite/batchingForReactDom'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fas } from "@fortawesome/free-solid-svg-icons"
 import { fab } from "@fortawesome/free-brands-svg-icons"

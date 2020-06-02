50b5a8d512d6d3a8d178e31a5f039bfe2acbe6f2
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-06-01 23:33:53 -0400

    add product to cart function working properly

--- store/src/stores/ShoppingCartStore.js
+++ store/src/stores/ShoppingCartStore.js
@@ -1,19 +1,17 @@
-import { observable, action, computed, decorate } from "mobx"
+import { observable, action, computed, decorate, toJS } from "mobx"
 
 export class ShoppingCartStore {
-  
   products = []
 
-  addProduct = (id,amount = 1) => {
-    this.products.push({
-      [id]:amount
-    })
-    this.viewProducts()
+  addProduct = (id, amount = 1) => {
+    const productIndex = this.products.map(e => e.id).indexOf(id)
+    
+    if(productIndex > -1){
+      this.products[productIndex].amount += amount
+    }else{
+      this.products.push({ id, amount })
+    }
   }
-  viewProducts = () => {
-    this.products.map(product=>console.log(product))
-  }
-
   get amountOfProducts() {
     return this.products.length
   }

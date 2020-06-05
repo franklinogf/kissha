1150721dccc657d91e0eb8f43a29bbc930876eb4
Miguel Angel <rampabonoide@gmail.com>
2020-06-05 15:11:17 -0400

    PasswordInput Completed! attached component CustomInput to PasswordInput

--- store/src/components/Inputs/PasswordInput.js
+++ store/src/components/Inputs/PasswordInput.js
@@ -1,18 +1,87 @@
-import React ,{useState} from "react"
+import React, { Fragment, useState, useRef } from "react"
 import { Form, Collapse } from "react-bootstrap"
 import styled from "styled-components"
+import CustomInput from './CustomInput'
 
 const FormInput = styled(Form.Control)`
   height: 48px;
 `
 
-const PasswordInput = ()=> {
+const PasswordInput = () => {
 
-  const [invalidPass,setInvalidPass] = useState()
-  const [invalidConfirmPass,setInvalidConfirmPass] = useState()
+    //STATES
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [invalidPassText, setInvalidPassText] = useState(false)
+    const [invalidConfirmPassText, setInvalidConfirmPassText] = useState(false)
+
+
+  //REFS
+  const passwordRef = useRef(null)
+  const confirmPasswordRef = useRef(null)
+
+  //HANDLERS
+  const handlePassword = () => {
+    //taking refs
+    let tmpPass = passwordRef.current.value
+    let tmpConfirmPass = confirmPasswordRef.current.value
+
+    //empty and invalid evaluation for PASSWORD
+    if (tmpPass === "") {
+        setPassword("")
+        setInvalidPassText(false)
+    }else if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(tmpPass)){
+        setPassword("invalid")
+        setInvalidPassText(true)
+    }else{
+        setPassword("ok")
+        setInvalidPassText(false)
+    }
+
+    //match evaluation CONFIRM PASSWORD
+    if (tmpPass !== "" && tmpConfirmPass !== "") {
+        if (tmpPass !== tmpConfirmPass) {
+            setConfirmPassword("invalid")
+            setInvalidConfirmPassText(true)
+        }else{
+            setConfirmPassword("ok")
+            setInvalidConfirmPassText(true)
+        }
+    } else {
+        setConfirmPassword("")
+        setInvalidConfirmPassText(false)
+    }
+  }
+
+  const onChangePassword = () => {
+    const tmpPass = passwordRef.current.value
+    let tmpConfirmPass = confirmPasswordRef.current.value
+
+    password === "invalid" && tmpPass !== ""
+      ? /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(tmpPass) &&
+        setPassword("ok")
+      : setPassword("")
+
+    confirmPassword === "invalid" && tmpConfirmPass !== ""
+      ? tmpPass === tmpConfirmPass && setConfirmPassword("ok")
+      : setConfirmPassword("")
+  }
 
   return (
-   <></>
+    <Fragment>
+      <CustomInput 
+        id="password"
+        input={["password",password, handlePassword,onChangePassword,null,passwordRef]}
+        label={["Password"]}
+        collapses={[["Invalid Password",invalidPassText]]}
+      />
+        <CustomInput 
+        id="confirmPassword"
+        input={["password",confirmPassword, handlePassword,onChangePassword,null,confirmPasswordRef]}
+        label={["Confirm Password","inside"]}
+        collapses={[["Password doesn't match",invalidConfirmPassText]]}
+      />
+    </Fragment>
   )
 }
 

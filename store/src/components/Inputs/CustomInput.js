b0ff2f10beb2ae1479be9aa39b42319cf19b2c6e
Miguel Angel <rampabonoide@gmail.com>
2020-06-05 15:09:04 -0400

    comments for CustomInput, for better understanding

--- store/src/components/Inputs/CustomInput.js
+++ store/src/components/Inputs/CustomInput.js
@@ -7,17 +7,20 @@ const FormInput = styled(Form.Control)`
 `
 
 const CustomInput = props => {
-  const controlId = props.id
-  const extraClassName = props.className || ""
+  const controlId = props.id // MUST
+  const extraClassName = props.className || "" //OPTIONAL
   const [
-    inputType,
-    inputState,
-    onBlur,
-    onChange,
-    inputPlaceholder,
-    inputRef,
+    inputType, //1st parameter MUST
+    inputState, //2nd parameter OPTIONAL
+    onBlur, //3rd parameter OPTIONAL
+    onChange, //4th parameter OPTIONAL
+    inputPlaceholder, //5th parameter OPTIONAL
+    inputRef, //6th parameter OPTIONAL
   ] = props.input
-  const [labelText, labelEffect = "inside-to-outside"] = props.label
+  const [
+    labelText, //1st parameter MUST if you invoke this prop
+    labelEffect, //2nd parameter OPTIONAL if you invoke this prop
+  ] = props.label
 
   return (
     <Form.Group
@@ -41,10 +44,25 @@ const CustomInput = props => {
         ref={inputRef && inputRef}
       />
       {props.label && (
-        <Form.Label className={`_label-${labelEffect}`}>{labelText}</Form.Label>
+        <Form.Label
+          className={`_label-${
+            labelEffect ? labelEffect : "inside-to-outside"
+          }`}
+        >
+          {labelText}
+        </Form.Label>
       )}
+
+      {
+          //COLLAPSES
+          // you can put as many collapses as you want, in the component declaration you need to insert each collapse as an array in another array [[collapse1],[collapse2],etc...]
+          // 1st parameter (TEXT) a MUST if you invoke this prop
+          // 2nd parameter (IN) OPTIONAL if you invoke this prop
+          // 3rd parameter (TEXT COLOR) OPTIONAL if you invoke this prop
+      }
+
       {props.collapses &&
-        props.collapses.map((collapse,i) => (
+        props.collapses.map((collapse, i) => (
           <Collapse key={i} in={collapse[1] && collapse[1]}>
             <div
               className={`_font-size-13 text-${

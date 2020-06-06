9719dad4b7cf50af0d7484f45e388a5f492fc0d2
Miguel Angel <rampabonoide@gmail.com>
2020-06-05 21:18:07 -0400

    attached CustomInputs to Login, only need fetch to login and register

--- store/src/components/Users/Login.js
+++ store/src/components/Users/Login.js
@@ -1,35 +1,18 @@
-import React, { Fragment } from "react"
+import React from "react"
 import Section from "../Layout/Section"
 import { Form, Row, Col } from "react-bootstrap"
-import styled from "styled-components"
 import Btn from "../Buttons/Button"
 import { Link } from "gatsby"
-
-const FormInput = styled(Form.Control)`
-  height: 48px;
-`
+import CustomInput from '../Inputs/CustomInput'
 
 const Login = () => {
   return (
-    <div className="bg-light p-3 rounded-lg" style={{width:'23rem'}}>
+    <div className="bg-light p-3 rounded-lg" style={{ width: "23rem" }}>
       <Section.Header title="Login" fontSize={32} />
       <Form>
-        <Form.Group controlId="email">
-          <FormInput
-            type="email"
-            placeholder="Enter email or number phone"
-            className="_font-Montserrat _font-size-14 py-2"
-          />
-          <Form.Text className="text-muted"></Form.Text>
-        </Form.Group>
+        <CustomInput id="emailOrPhone" input={["text"]} label={["Enter Email or Phone Number"]} />
+        <CustomInput id="password" input={["password"]} label={["Password"]} />
 
-        <Form.Group controlId="password" className="mb-4">
-          <FormInput
-            type="password"
-            placeholder="Password"
-            className="_font-Montserrat _font-size-14"
-          />
-        </Form.Group>
         <Row className="justify-content-between">
           <Col xs={12} lg={2} className="align-self-end">
             <Btn size="lg" fontSize={16}>

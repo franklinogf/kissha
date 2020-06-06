1b4c003de3f46d5ef510cbc183f18bbfb0aee162
Miguel Angel <rampabonoide@gmail.com>
2020-06-05 20:49:59 -0400

    component NameInput done. you can use everywhere as you wish

--- store/src/components/Inputs/NameInput.js
+++ store/src/components/Inputs/NameInput.js
@@ -1,10 +1,44 @@
 import React, { Fragment, useState, useRef } from "react"
 import CustomInput from "./CustomInput"
 
-const PasswordInput = () => {
+const NameInput = ({ idName, labelName, labelEffect }) => {
+  const [nameStatus, setNameStatus] = useState("")
+  const [collapseText, setCollapseText] = useState(false)
 
-    return(
-        <></>
-    )
+  const handleName = e => {
+    const tmpInput = e.target.value
 
+    if (tmpInput === "") {
+      setNameStatus("")
+      setCollapseText(false)
+      return
+    }
+
+    if (!/^[a-zA-Z ]+$/.test(tmpInput)) {
+      setNameStatus("invalid")
+      setCollapseText(true)
+      return
+    } else {
+      setNameStatus("ok")
+      setCollapseText(false)
+    }
+  }
+
+  return (
+    <CustomInput
+      id={idName}
+      input={["text", nameStatus, handleName]}
+      label={[labelName ? labelName : null, labelEffect ? labelEffect : null]}
+      collapses={[
+        [
+          `Invalid ${
+            labelName ? labelName : "input"
+          }, don't use Numbers or special chars`,
+          collapseText,
+        ],
+      ]}
+    />
+  )
 }
+
+export default NameInput

392c892c1dff87ca3cca3bf32146b19b3b54fc85
Miguel Angel <rampabonoide@gmail.com>
2020-06-07 04:22:59 -0400

    signup.js now can post a new user to the DB, need to polish the submit

--- store/src/components/Users/Register.js
+++ store/src/components/Users/Register.js
@@ -1,21 +1,84 @@
-import React from "react"
+import React, { useState } from "react"
 import Section from "../Layout/Section"
 import { Form } from "react-bootstrap"
 import Btn from "../Buttons/Button"
 import EmailInput from "../Inputs/EmailInput"
 import PasswordInput from "../Inputs/PasswordInput"
 import NameInput from "../Inputs/NameInput"
+import AxiosClient from "../../config/axios"
 
 const Register = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [firstNameStatus, setFirstNameStatus] = useState("")
+  const [lastNameStatus, setLastNameStatus] = useState("")
+  const [newUser, setNewUser] = useState({
+    firstName: "",
+    lastName: "",
+    password: "",
+    email: "",
+  })
+
+  //
+  const handleNewUser = e => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    //check validations
+    if (
+      email === "ok" &&
+      password === "ok" &&
+      firstNameStatus === "ok" &&
+      lastNameStatus === "ok"
+    ) {
+      console.log("todo correcto")
+      //POST REQUEST
+      AxiosClient.post('/users',newUser)
+        .then(response=>{
+          console.log(response)
+        })
+    } else {
+      console.log("something was wrong")
+      //GENERATE ALERT WRONG INFO
+    }
+  }
+
   return (
     <div className="bg-light rounded-lg p-3" style={{ width: "23rem" }}>
-      <Section.Header title="Sign Un" fontSize={32} />
+      <Section.Header title="Sign Up" fontSize={32} />
       <Form>
-        <EmailInput/>
-        <PasswordInput enableConfirm/>
-        <NameInput idName="firstName" labelName="First Name"/>
-        <NameInput idName="lastName" labelName="Last Name"/>
-        <Btn size="lg" fontSize={16}>
+        <EmailInput
+          email={email}
+          setEmail={setEmail}
+          onChangeHandler={handleNewUser}
+        />
+        <PasswordInput
+          password={password}
+          setPassword={setPassword}
+          onChangeHandler={handleNewUser}
+          enableConfirm
+        />
+        <NameInput
+          idName="firstName"
+          labelName="First Name"
+          nameStatus={firstNameStatus}
+          setNameStatus={setFirstNameStatus}
+          onChangeHandler={handleNewUser}
+        />
+        <NameInput
+          idName="lastName"
+          labelName="Last Name"
+          nameStatus={lastNameStatus}
+          setNameStatus={setLastNameStatus}
+          onChangeHandler={handleNewUser}
+        />
+        <Btn onClick={handleSubmit} size="lg" fontSize={16}>
           Sign Up
         </Btn>
       </Form>

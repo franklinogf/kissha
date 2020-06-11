f7f72c31ff643ae884aa0986fe2b0e3a4495d36f
Miguel Angel <rampabonoide@gmail.com>
2020-06-11 18:58:21 -0400

    added UserStore to Main Context

--- store/src/contexts/MainContext.js
+++ store/src/contexts/MainContext.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ShoppingCartStore } from '../stores/ShoppingCartStore'
+import { UserStore } from '../stores/UserStore'
 
 export const MainContext = React.createContext({
-   ShoppingCartStore: new ShoppingCartStore()
+   ShoppingCartStore: new ShoppingCartStore(),
+   UserStore: new UserStore()
 })

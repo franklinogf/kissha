48a08d86983902cc50db86acc2b2e260aa7eee31
Miguel Angel <rampabonoide@gmail.com>
2020-06-11 21:53:30 -0400

    .

--- api/server.js
+++ api/server.js
@@ -2,12 +2,15 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 const db = require('./database')
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
 
 const users = require('./routes/users')
 const address = require('./routes/address')
 const categories = require('./routes/categories')
 const products = require('./routes/products')
 const emails = require('./routes/emails')
+const login = require('./routes/login')
 require("dotenv").config({path:__dirname+'.env'})
 const port = process.env.PORT || 5000
 
@@ -15,7 +18,25 @@ db.authenticate()
     .then(() => console.log('DB Conectada'))
     .catch(error => console.log(error))
 
+
+//SESSION STORE
+const sessionStore = new MySQLStore({
+    expiration: (1825 * 86400 * 1000),
+    endConnectionOnClose: false
+}, db);
+
 // Middleware
+app.use(session({
+    key: '1234567890qwertyuiop',
+    secret: 'asdfghjklzxcvbnm',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1825 * 86400 * 1000),
+        httpOnly: false
+    }
+}));
 app.use(cors())
 app.use(express.json())
 app.use(['/users/:id',"/categories/:id","/address/:id"], (req, res, next) => {
@@ -35,6 +56,7 @@ app.use('/address', address)
 app.use('/categories', categories)
 app.use('/products', products)
 app.use('/emails', emails)
+app.use('',login)
 
 // Start Server
 app.listen(port, () => console.log(`
--- store/src/components/Users/Login.js
+++ store/src/components/Users/Login.js
@@ -1,21 +1,50 @@
-import React from "react"
+import React,{useState} from "react"
 import Section from "../Layout/Section"
 import { Form, Row, Col } from "react-bootstrap"
 import Btn from "../Buttons/Button"
 import { Link } from "gatsby"
-import CustomInput from '../Inputs/CustomInput'
+import CustomInput from "../Inputs/CustomInput"
+import AxiosClient from "../../config/axios"
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  })
+
+
+  const handleUser = e =>{
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleLogin = e =>{
+    // if everything ok
+
+     //POST REQUEST
+     AxiosClient.post('/login',user)
+     .then(response=>{
+       console.log(response)
+     })
+
+  }
+
   return (
     <div className="bg-light p-3 rounded-lg" style={{ width: "23rem" }}>
       <Section.Header title="Login" fontSize={32} />
       <Form>
-        <CustomInput id="emailOrPhone" input={["text"]} label={["Enter Email or Phone Number"]} />
-        <CustomInput id="password" input={["password"]} label={["Password"]} />
+        <CustomInput
+          id="email"
+          input={["text",null,null,handleUser]}
+          label={["Enter Email or Phone Number"]}
+        />
+        <CustomInput id="password" input={["password",null,null,handleUser]} label={["Password"]} />
 
         <Row className="justify-content-between">
           <Col xs={12} lg={2} className="align-self-end">
-            <Btn size="lg" fontSize={16}>
+            <Btn size="lg" fontSize={16} onClick={handleLogin}>
               Login
             </Btn>
           </Col>

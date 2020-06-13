d8134d96e5a93e8ae45a0fef4001813ab08fb81c
Miguel Angel <rampabonoide@gmail.com>
2020-06-13 08:56:59 -0400

    fixed part of the Login component

--- store/src/components/Users/Login.js
+++ store/src/components/Users/Login.js
@@ -1,34 +1,58 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import Section from "../Layout/Section"
-import { Form, Row, Col, Spinner } from "react-bootstrap"
+import { Form, Row, Col, Spinner, Alert, Collapse } from "react-bootstrap"
 import Btn from "../Buttons/Button"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import CustomInput from "../Inputs/CustomInput"
 import AxiosClient from "../../config/axios"
+import useStores from "../../hooks/useStores"
+import { observer } from "mobx-react"
 
-const Login = () => {
+const Login = observer(() => {
+  //globalState
+  const { UserStore } = useStores()
+
+  //states
+  const [isLoading, setLoading] = useState(false)
+  const [loginFail, setLoginFail] = useState({
+    status: false,
+    message: "",
+  })
   const [user, setUser] = useState({
     email: "",
     password: "",
   })
 
+  //lifecyle
 
-  const handleUser = e =>{
+  //handlers
+  const handleUser = e => {
     setUser({
       ...user,
       [e.target.name]: e.target.value,
     })
   }
 
-  const handleLogin = e =>{
+  const handleLogin = e => {
     // if everything ok
 
-     //POST REQUEST
-     AxiosClient.post('/login',user)
-     .then(response=>{
-       console.log(response)
-     })
-
+    setLoading(true)
+    //POST REQUEST
+    AxiosClient.post("/login", user).then(response => {
+      if (response.data.isLoggedIn) {
+        //Correctly logged, proceed to store info
+        UserStore.setLogin(response.data)
+        //redirect to userProfile or home
+        navigate("/")
+      } else {
+        //Wrong Login, show collapse
+        setLoginFail({
+          status: true,
+          message: response.data.message,
+        })
+      }
+    })
+    setLoading(false)
   }
 
   return (
@@ -37,19 +61,19 @@ const Login = () => {
       <Form>
         <CustomInput
           id="email"
-          input={["text",null,null,handleUser]}
+          input={["text", null, null, handleUser]}
           label={["Enter Email or Phone Number"]}
         />
-        <CustomInput id="password" input={["password",null,null,handleUser]} label={["Password"]} />
+        <CustomInput
+          id="password"
+          input={["password", null, null, handleUser]}
+          label={["Password"]}
+        />
 
         <Row className="justify-content-between">
           <Col xs={12} lg={2} className="align-self-end">
-            <Btn onClick={handleSubmit} size="lg" fontSize={16}>
-            {!isLoading ? (
-              "Login"
-            ) : (
-              <Spinner animation="border" size="sm" />
-            )}
+            <Btn onClick={handleLogin} size="lg" fontSize={16}>
+              {!isLoading ? "Login" : <Spinner animation="border" size="sm" />}
             </Btn>
           </Col>
           <Col xs={12} lg={9}>
@@ -68,9 +92,26 @@ const Login = () => {
             </Link>
           </Col>
         </Row>
+        <Collapse in={loginFail.status} className="mt-2">
+          <div id="example-collapse-text">
+            <Alert
+              variant="danger"
+              onClose={() =>
+                setLoginFail({
+                  ...loginFail,
+                  status: false,
+                })
+              }
+              dismissible
+            >
+              <Alert.Heading>Ops!</Alert.Heading>
+              <p>{loginFail.message}</p>
+            </Alert>
+          </div>
+        </Collapse>
       </Form>
     </div>
   )
-}
+})
 
 export default Login

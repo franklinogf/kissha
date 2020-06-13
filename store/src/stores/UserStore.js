aa29c14d7dac551916cdffdedb60c631fd714be4
Miguel Angel <rampabonoide@gmail.com>
2020-06-13 08:58:00 -0400

    set asyncComputed to the UserStore, NEED TO ADD SESSION TO THE SERVER LATER

--- store/src/stores/UserStore.js
+++ store/src/stores/UserStore.js
@@ -1,17 +1,48 @@
 import { observable, action, computed, decorate } from "mobx"
-
+import { asyncComputed } from "computed-async-mobx"
+import AxiosClient from "../config/axios"
 
 export class UserStore {
+  user = JSON.parse(localStorage.getItem("loggedUser")) || {
+    isLoggedIn: false,
+    id: 0,
+  }
 
-    user = {
-        isLoggedIn: false,
-        firstName: '',
-        lastName:'',
-        email:''
+  setLogin = newUser => {
+    if (newUser.isLoggedIn && newUser.id) {
+      this.user = newUser
+      localStorage.setItem("loggedUser", JSON.stringify(this.user))
     }
+  }
+
+  findUser = asyncComputed({}, 600, async () => {
+    const response = await AxiosClient.get(
+      `/users/${this.user.id}/limited`
+    ).then(response => response.data.data)
+    return response
+  })
+
+  get obtainUser() {
+    return this.findUser.get()
+  }
+
+  get isLogged() {
+    return this.user.isLoggedIn
+  }
+
+  setLogout = () => {
+    this.user = {
+      isLoggedIn: false,
+      id: 0,
+    }
+    localStorage.setItem("loggedUser", JSON.stringify(this.user))
+  }
 }
 
-decorate(UserStore,{
-    user: observable
-});
-
+decorate(UserStore, {
+  user: observable,
+  setLogin: action,
+  logout: action,
+  obtainUser: computed,
+  isLogged: computed,
+})

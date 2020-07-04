91dc39f4a3dd6adbd58d9b50af18d1c617188e87
Miguel Angel <rampabonoide@gmail.com>
2020-07-04 10:51:17 -0400

    added the user path in the gatsby-config

--- store/gatsby-config.js
+++ store/gatsby-config.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/user/*`] },
+    },
   ],
 }

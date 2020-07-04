57cde073db30b03fcc222950bab25dc3bf33031b
Miguel Angel <rampabonoide@gmail.com>
2020-07-04 11:11:31 -0400

    set the authentication before load the page

--- store/src/components/Layout/MainLayout.js
+++ store/src/components/Layout/MainLayout.js
@@ -1,22 +1,63 @@
-import React from "react"
+import React, { useEffect, Fragment } from "react"
 import { Container } from "react-bootstrap"
 import Footer from "./Footer"
 import Header from "./Header"
 import useSticky from "../../hooks/useSticky"
+import useStores from "../../hooks/useStores"
+import AxiosClient from "../../config/axios"
+import { observer } from "mobx-react"
 
-export const MainLayout = ({ children }) => {
-  const {isSticky, element} = useSticky()
+export const MainLayout = observer(({ children }) => {
+  const { UserStore } = useStores()
+  const { isSticky, element } = useSticky()
+
+  const fetchData = async () => {
+    AxiosClient.get(`/isLogged`).then(response => {
+      if (response.data.status) {
+        UserStore.setLogin(true)
+        UserStore.setUser(response.data.data)
+      } else {
+        UserStore.setLogin(false)
+        UserStore.setUser({})
+      }
+    })
+  }
+
+  //useEffect didmount
+  useEffect(() => {
+    for (let i = 0; i < 1; i++) {
+      fetchData()
+    }
+  })
+
+  //useEffect didupdate
+  useEffect(() => {
+    for (let i = 0; i < 1; i++) {
+      fetchData()
+    }
+
+    if (UserStore.loginStatus) {
+      fetchData()
+    } else {
+      UserStore.setUser({})
+    }
+  }, [UserStore.loginStatus])
+
   return (
-    <>
-      <Container className="pt-3">
-        <Header sticky={isSticky} />
-      </Container>
-      <Container ref={element} fluid className="p-0">
-        {children}
-      </Container>
-      <Footer />
-    </>
+    <div>
+      {UserStore.loginStatus !== null && (
+        <Fragment>
+          <Container className="pt-3">
+            <Header sticky={isSticky} />
+          </Container>
+          <Container ref={element} fluid className="p-0">
+            {children}
+          </Container>
+          <Footer />
+        </Fragment>
+      )}
+    </div>
   )
-}
+})
 
 export default MainLayout

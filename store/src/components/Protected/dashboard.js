d67ed3bd3b1de3114a39fe7bb3e10f052c7d4cbd
Miguel Angel <rampabonoide@gmail.com>
2020-07-13 23:52:25 -0400

    infoUserDeleted

--- store/src/components/Protected/dashboard.js
+++ store/src/components/Protected/dashboard.js
@@ -1,53 +1,65 @@
-import React, { Component, Fragment } from "react"
+import React, { useState, Fragment } from "react"
+import useStores from "../../hooks/useStores"
+import AxiosClient from "../../config/axios"
 import Section from "../Layout/Section"
 import PageTitle from "../Layout/PageTitle"
 import DashboardNavbar from "../Users/DashboardNavbar"
 import AccountSettings from "../Users/AccountSettings"
 import AddressSettings from "../Users/AddressSettings"
-import InfoUser from "../Users/InfoUser"
 import OrdersSettings from "../Users/OrdersSettings"
 import LoginSettings from "../Users/LoginSettings"
 import PaymentSettings from "../Users/PaymentSettings"
+import LoadingSection from "../Layout/LoadingSection"
+import { observer } from "mobx-react"
 
-export default class Dashboard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selectedSettings: "info",
-    }
-    this.handleSelectedOption = this.handleSelectedOption.bind(this)
-  }
-  handleSelectedOption(eventKey) {
-    this.setState({
-      selectedSettings: eventKey,
-    })
-  }
+const Dashboard = observer(() => {
+  const { UserStore } = useStores()
 
-  render() {
-    const selected = this.state.selectedSettings
-    return (
-      <Fragment>
-        <PageTitle title="Dashboard" />
-        <Section bg=" _color-sweet" height={185} padding="pt-3 pb-0 px-0">
-          <div className="d-flex justify-content-center">
-            <span className=" _logo-text text-primary _font-size-36">K </span>
-          </div>
+  //states
+  const [selectedSettings, setSelectedSettings] = useState("account")
 
-          <div className="d-flex justify-content-center mb-4 mt-2 _font-size-30 _font-Montserrat font-italic">
-            <h1 className="font-weight-light">Welcome Alexa!</h1>
-          </div>
-          <DashboardNavbar
-            selectedLink={selected}
-            setSelectedLink={this.handleSelectedOption}
-          />
-        </Section>
-        {selected === "info" && <InfoUser />}
-        {selected === "account" && <AccountSettings />}
-        {selected === "address" && <AddressSettings />}
-        {selected === "orders" && <OrdersSettings />}
-        {selected === "security" && <LoginSettings />}
-        {selected === "payment" && <PaymentSettings />}
-      </Fragment>
+  //handlers
+  const handleSelectedOption = eventKey => {
+    //prepare the view
+    setSelectedSettings("loading")
+
+    //before load the component, fetch the info to fill the data
+    AxiosClient.get(`/users/${UserStore.obtainUser.id}/limited`).then(
+      response => {
+        UserStore.setUser(response.data.data)
+        setSelectedSettings(eventKey)
+      }
     )
   }
-}
+
+  return (
+    <Fragment>
+      <PageTitle title="Dashboard" />
+      <Section bg=" _color-sweet" height={185} padding="pt-3 pb-0 px-0">
+        <div className="d-flex justify-content-center">
+          <span className=" _logo-text text-primary _font-size-36">K </span>
+        </div>
+
+        <div className="d-flex justify-content-center mb-4 mt-2 _font-size-30 _font-Montserrat font-italic">
+          <h1 className="font-weight-light">
+            Welcome {UserStore.obtainUser.firstName}!
+          </h1>
+        </div>
+        <DashboardNavbar
+          selectedLink={selectedSettings}
+          setSelectedLink={handleSelectedOption}
+        />
+      </Section>
+      {selectedSettings === "loading" && (
+        <LoadingSection bg=" _color-candy-blue" />
+      )}
+      {selectedSettings === "account" && <AccountSettings />}
+      {selectedSettings === "address" && <AddressSettings />}
+      {selectedSettings === "orders" && <OrdersSettings />}
+      {selectedSettings === "security" && <LoginSettings />}
+      {selectedSettings === "payment" && <PaymentSettings />}
+    </Fragment>
+  )
+})
+
+export default Dashboard

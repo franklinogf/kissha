26b154d0a9f11f13dbbb810d75405e16e804f46d
Miguel Angel <rampabonoide@gmail.com>
2020-07-13 23:43:32 -0400

    phone route created, manages phone changes of the user

---
+++ api/routes/phones.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const table = require('../models/users')
+
+
+router.get('/', (req, res) => {
+    table.findAll({
+        attributes: ['id','phone']
+    }).then(data => {
+        if (data.length > 0) {
+            res.json({
+                status: true,
+                message: "Ok",
+                data: data
+            })
+        } else {
+            res.json({
+                status: false,
+                message: `No data found`
+            })
+        }
+    })
+
+})
+
+module.exports = router

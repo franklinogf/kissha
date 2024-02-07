d648d831808440b88c46a0911b596f4e8d94cd80
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-06 21:33:47 -0500

    removed eslint and now using standard as linter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout ({
   children
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
